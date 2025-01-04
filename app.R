e172ab33cb44cf9cc9cd7739d13e2748d26e4f30
Thomas Elton <thomaselton.au@gmail.com>
2025-01-04 19:33:16 +1100

    Implemented null hypothesis section for 1-sample z-test.

--- app.R
+++ app.R
@@ -7,6 +7,8 @@ source("Utility/load_data_1_sample_srv.R")
 source("Utility/load_data_1_sample_ui.R")
 
 options(shiny.autoreload = TRUE)
+options(shiny.reactlog = TRUE)
+
 
 ui <- dashboardPage(
   skin = "black",
@@ -134,9 +136,113 @@ ui <- dashboardPage(
         
         conditionalPanel(
           condition = 'output.render_rest_of_exercise',
-          h1("IT WORKED")
+          
+              ############ SECTION: The NULL Hypothesis - Setting up the Box ############
+              fluidRow(
+                column(7,
+                       box(
+                         title = HTML("<u><b>The 'NULL' Hypothesis - Setting up the Box</b></u>"),
+                         status = "primary",
+                         width = "100%",
+                         solidHeader = FALSE,
+                         
+                         p("We start by using the box model to represent our null hypothesis."),
+                         
+                         # Step 1: Specify Population Standard Deviation
+                         box(
+                           title = "Step 1) Specify Population Standard Deviation (σ)",
+                           width = "100%",
+                           collapsible = TRUE,
+                           collapsed = FALSE,
+                           status = "info",
+                           solidHeader = FALSE,
+                           withMathJax(HTML("<p>An assumption for the 1-sample z-test is that the standard deviation of the population (denoted \\(\\sigma\\)) in which our sample is drawn is known. For example,
+                                a prior research paper may have estimated the population standard deviation, and you could use the value they discovered.<br><br>
+                                
+                                For this exercise, you can manually set the population standard deviation. However, for the most part, it is likely that the population standard
+                                deviation is unknown. Hence, for the purposes of this exercise, you can set the population standard deviation to be equal to the sample standard
+                                deviation (this has been automatically done below, but you are free to change the value). In practice, this is far from ideal, and in a future exercise, we will use the 1-sample t-test when the population standard deviation
+                                is unknown.</p>")),
+                           HTML("<br>"),
+                           fluidRow(
+                             column(7,
+                                    actionButton(
+                                      inputId = "set_pop_sd_to_sample",
+                                      label = "Set Population SD to be Sample SD",
+                                      class = "btn-primary",
+                                      style = "color: #fff;",
+                                      width = "100%"
+                                    )
+                             ),
+                             column(2,
+                                    withMathJax(HTML("<p style='font-size: 16px; text-align: right;'>\\(\\sigma = \\)</p>"))
+                             ),
+                             column(3,
+                                    uiOutput("pop_sd_numeric_input") 
+                             ),
+                           ),
+                         ),
+                         
+                         # Step 2: Specify NULL Hypothesis
+                         box(
+                           title = "Step 2) Specify NULL Hypothesis",
+                           width = "100%",
+                           collapsible = TRUE,
+                           collapsed = TRUE,
+                           status = "info",
+                           solidHeader = FALSE,
+                           HTML("<p>Next we need to specify the 'null' hypothesis. In this test we are focusing on the mean, so the null hypothesis is that the population mean
+                                  is equal to some value which we set below.</p>"),
+                           
+                           withMathJax(HTML("<p style='font-size: 16px; text-align: right;'>Given \\(\\mu \\), where \\(\\mu \\) is the mean of some variable:</p>")),
+                           fluidRow(
+                             column(7),
+                             column(2,
+                                    withMathJax(HTML("<p style='font-size: 16px; text-align: right;'>\\( H_0: \\) \\(\\mu = \\)</p>"))
+                             ),
+                             column(3,
+                                    numericInput(
+                                      "null_mu",
+                                      NULL,
+                                      value = 0
+                                    ),
+                             ),
+                           ),
+                         ),
+                         
+                         
+                         # Other
+                         box(
+                           title = "Other",
+                           width = "100%",
+                           collapsible = TRUE,
+                           collapsed = TRUE,
+                           status = "info",
+                           solidHeader = FALSE,
+                           HTML("<p>
+                           <ul>
+                              <li>The value for <b>n</b> comes from the number of values in the sample chosen above.</li>
+                              <li>The <b>observed value (OV)</b> comes from the mean of the sample chosen above.</li>
+                           </ul></p>")
+                         )
+                       )
+                ),
+                column(5,
+                       # Box model output.
+                       box(
+                         solidHeader = TRUE,
+                         width = "100%",
+                         HTML("<center>"),
+                         grVizOutput("box_model", width = "70%", height = "70%"),
+                         HTML("</center>")
+                       ),
+                )
+              ),
+          
+          
+              HTML("<br><br><br>"),
+
         )
-        
         
       ),
       
@@ -155,14 +261,80 @@ server <- function(input, output, session) {
 
   sample_data_session <- load_1_sample_data_Server(id = "loading_data")
   
+  # Store the sample data.
+  sample_data <- reactiveVal(NULL)
+  
   # Variable that is true when the data has been specified, meaning the rest of the exercise can commence
   # This variable can be accessed by the ui conditional panel.
   output$render_rest_of_exercise <- reactive({
     !is.null(sample_data_session$data())
+    sample_data(sample_data_session$data())
   })
   outputOptions(output, "render_rest_of_exercise", suspendWhenHidden = FALSE)
   
   ################################################################
+  
+  # Numeric text field to enter the population sd.
+  output$pop_sd_numeric_input <- renderUI({
+    sample_sd = sd(sample_data() ,na.rm = TRUE)
+    numericInput(
+      "population_standard_deviation_numeric",
+      NULL,
+      value = sample_sd,
+      min = 0
+    )
+  })
+  
+  # Button logic to set the pop sd to the sample sd.
+  observeEvent(input$set_pop_sd_to_sample, {
+    req(input$population_standard_deviation_numeric)
+    sample_sd = sd(sample_data() ,na.rm = TRUE)
+    updateNumericInput(
+      session,
+      inputId = "population_standard_deviation_numeric",
+      value = sample_sd
+    )
+  })
+  
+  # Override the sd value to be equal to the sample sd if it is less than or equal to 0.
+  observeEvent(input$population_standard_deviation_numeric, {
+    if (is.na(input$population_standard_deviation_numeric) || input$population_standard_deviation_numeric <= 0) {
+      sample_sd = sd(sample_data() ,na.rm = TRUE)
+      updateNumericInput(
+        session,
+        inputId = "population_standard_deviation_numeric",
+        value = sample_sd
+      )
+    }
+  })
+  
+  # Box model plot
+  output$box_model <- renderGrViz({
+    
+    # String with mu and sigma.
+    pop_details = paste("&mu; = ", as.character(round(input$null_mu, digits = 3)), "; &sigma; = ", as.character(round(input$population_standard_deviation_numeric, digits = 3)))
+    
+    # Set up graph and box
+    diagram = "digraph diagram { graph [layout = dot, rankdir = TB] node [shape = box, style = filled, fillcolor = \"#bdfeff\", fontsize = 12, width = 2.5] box [label = '"
+    diagram = paste(diagram, pop_details, "']", sep = "")
+    
+    # Set up sample circle.
+    diagram = paste(diagram, " node [shape = oval,width = 1.5,fillcolor = \"#f9ffbd\", fontsize = 12] sample [label = '", "OV = ",  
+                    as.character(round(mean(sample_data(), na.rm = TRUE), digits = 3)), "']", sep = "")
+    
+    # Create edge between box and circle.
+    # Annotate edge with n value.
+    n = length(sample_data())
+    diagram = paste(diagram, " edge [minlen = 2] box->sample [label = '  n = ", n, "', fontsize = 12, labeldistance = 5]}", sep = "")
+    
+    return (grViz(diagram))
+    
+  })
+  
+  
+  
+  
+  
   
   boxModelMainServer(id = "box_model")
   
