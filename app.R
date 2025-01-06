6916abd03c9c9d1ba019b755aeacb7a82eeb1eda
Thomas Elton <thomaselton.au@gmail.com>
2025-01-06 11:59:23 +1100

    Added test statistic section to the 1-sample z-test exercise.

--- app.R
+++ app.R
@@ -341,6 +341,31 @@ ui <- dashboardPage(
               ),
               
               HTML("<br><br><br>"),
+          
+              ############ SECTION: Test Statistics ############
+              fluidRow(
+                column(12,
+                       box(
+                         title = HTML("<u><b>Test Statistic</b></u>"),
+                         status = "primary",
+                         width = "100%",
+                         solidHeader = FALSE,
+                         
+                         fluidRow(
+                           column(6,
+                                  HTML("<p><b>Step 1) Calculate Expected Value (SE) and Standard Error (SE)</b></p>"),
+                                  uiOutput("ev_and_se_text")
+                           ),
+                           column(6,
+                                  HTML("<p><b>Step 2) Test Statistic Calculation</b></p>"),
+                                  uiOutput("test_statistic_calculation")
+                           )
+                         ),
+                       ),
+                ),
+              ),
+              
+              HTML("<br><br><br>"),
 
         )
         
@@ -464,6 +489,65 @@ server <- function(input, output, session) {
     )
   })
   
+  EV_string = reactiveVal("")
+  SE_string = reactiveVal("")
+  
+  # Expected value and standard error output.
+  output$ev_and_se_text <- renderUI({
+    
+    sample_size = length(sample_data())
+    
+    # Find EV and SE.
+    mean_ = input$null_mu
+    sd_ = input$population_standard_deviation_numeric
+    EV = mean_
+    SE = sd_ / sqrt(sample_size)
+
+    EV_string(as.character(round(EV, 5)))
+    SE_string(as.character(round(SE, 5)))
+    
+    expected_value = withMathJax(
+      HTML("<p>Expected Value:</p>"),
+      HTML(paste("$$\\begin{align*} \\text{EV} &= \\mu \\\\ &=", EV_string(), "\\end{align*}$$", sep = ""))
+    )
+    
+    standard_error = withMathJax(
+      HTML("<p>Standard Error:</p>"),
+      HTML(paste("$$\\begin{align*} \\text{SE} &= \\frac{\\sigma}{\\sqrt{n}} \\\\ &= \\frac{", round(sd_, 5) , "}{\\sqrt{", 
+                 as.character(sample_size), "}}\\\\ &= ", SE_string(), "\\end{align*}$$", sep = ""))
+    )
+
+    return(
+      tagList(
+        expected_value, standard_error
+      )
+    )
+  })
+  
+  test_stat = reactiveVal("")
+  
+  # Test statistic output.
+  output$test_statistic_calculation <- renderUI({
+    
+    observed_val = mean(sample_data(), na.rm = TRUE)
+    
+    # Calculate test statistic.
+    temp = (observed_val - as.numeric(EV_string()))/as.numeric(SE_string())
+    temp = as.character(round(temp, 4))
+    test_stat(temp)
+    
+    t_stat = withMathJax(
+      HTML(paste("$$\\begin{align*} \\text{TS} &= \\frac{\\text{OV} - \\text{EV}}{\\text{SE}} \\\\ &= \\frac{", as.character(round(observed_val,5)), " - ", 
+                 EV_string(), "}{", SE_string(), "} \\\\ &= ", test_stat(), "\\end{align*}$$", sep = ""))
+    )
+    additional_line = HTML(paste("<p style = 'text-align: left;'><span style='color: blue;'><i>The value for the test-statistic is ", test_stat(), ". </i></span></p>", sep = ""))
+    return(
+      tagList(
+        t_stat,
+        additional_line
+      )
+    )
+  })
   
   
   
