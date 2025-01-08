4b5843780a818ea2138b2c78947311e75ea16dc6
Thomas Elton <thomaselton.au@gmail.com>
2025-01-08 14:55:03 +1100

    Added conclusion to the 1-sample z-test.

--- z-test/1_sample_z_test_srv.R
+++ z-test/1_sample_z_test_srv.R
@@ -309,6 +309,51 @@ oneSampleZTestServer <- function(id) {
     output$test_stat_normal_plot = renderPlot({
       return(curve_shaded_test_stat(dnorm, list(mean = 0, sd = 1), as.numeric(test_stat()), input$alternate_hypothesis_choice))
     })
+    
+    # Process significance level text input.
+    alpha = reactiveVal(0.05)
+    alpha_warning = reactiveVal(FALSE)
+    observeEvent(input$alpha_value, {
+      if (is.na(input$alpha_value) || input$alpha_value < 0 || input$alpha_value > 1) {
+        alpha(0.05)
+        alpha_warning(TRUE)
+      } else {
+        alpha(input$alpha_value)
+        alpha_warning(FALSE)
+      }
+    })
+    
+    # Error message for when the value for alpha is invalid.
+    output$significance_level_warning <- renderUI({
+      if (alpha_warning()) {
+        return(
+          HTML("<span style='color: red;'><p>Error: The value for α must be between 0 and 1.</p></span>")
+        )
+      }
+    })
+    
+    # Hypothesis test output
+    output$conclusion_output <- renderUI({
+      
+      # Change > or < sign depending on whether the p-value is less than or greater than alpha.
+      math_line = withMathJax(
+        HTML(paste("$$\\begin{align*} \\alpha &> p \\\\", as.character(alpha()), " &> ", as.character(round(p_val(), 4)), "\\end{align*}$$", sep = ""))
+      )
+      conclusion_line = HTML("<span style='color: blue;'><p>As the p value is less than our significance level, we <b>reject the null hypothesis</b>.</p></span>")
+      if (p_val() > alpha()) {
+        math_line = withMathJax(
+          HTML(paste("$$\\begin{align*} \\alpha &< p \\\\", as.character(alpha()), " &< ", as.character(round(p_val(), 4)), "\\end{align*}$$", sep = ""))
+        )
+        conclusion_line = HTML("<span style='color: blue;'><p>As the p value is greater than our significance level, we <b>accept the null hypothesis</b>.</p></span>")
+      }
+      
+      return(
+        tagList(
+          math_line,
+          conclusion_line
+        )
+      )
+    })
   
     
 
--- z-test/1_sample_z_test_ui.R
+++ z-test/1_sample_z_test_ui.R
@@ -276,6 +276,51 @@ oneSampleZTestUI <- function(id) {
 
           HTML("<br><br><br><br><br><br><br>"),
       
+          ############ SECTION:Conclusion ############
+          fluidRow(
+            column(12,
+                   box(
+                     title = HTML("<u><b>Conclusion</b></u>"),
+                     status = "primary",
+                     width = "100%",
+                     solidHeader = FALSE,
+                     
+                     fluidRow(
+                       
+                       # Section to enter significance level.
+                       column(6,
+                              HTML("<p><b>Step 1) What is your significance level</b>?</p>"),
+                              
+                              # Space to enter significance value.
+                              fluidRow(
+                                column(1,
+                                       withMathJax(HTML("<p style='font-size: 16px; text-align: right;'>\\( \\alpha = \\)</p>"))
+                                ),
+                                column(3,
+                                       numericInput(
+                                         ns("alpha_value"),
+                                         NULL,
+                                         value = 0.05,
+                                         min = 0,
+                                         max = 1,
+                                         width = "100%"
+                                       ),
+                                ),
+                              ),
+                              uiOutput(ns("significance_level_warning")),
+                       ),
+                       
+                       # Section to provide final result.
+                       column(6,
+                              HTML("<p><b>Step 2) Final Conclusion</b></p>"),
+                              uiOutput(ns("conclusion_output")),
+                       )
+                     )
+                   )
+            ),
+          )
+      
+      
     )
     
   )
