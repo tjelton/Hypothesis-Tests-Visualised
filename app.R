a5f0ee925f3c1761ba16dda9f64f0d5d2905eb85
Thomas Elton <thomaselton.au@gmail.com>
2025-08-11 19:24:48 +1000

    Finished confidence interval lesson content.

--- app.R
+++ app.R
@@ -90,7 +90,6 @@ ui <- page_navbar(
                      HTML("<br><br><br>"),
                      
                      fluidRow(
-                       
                        column(5,
                               tight_card(
                                 "Simulating Confidence Intervals",
@@ -152,6 +151,156 @@ ui <- page_navbar(
                        
                      ),
                      
+                     HTML("<br><br><br>"),
+                     
+                     fluidRow(
+                       column(5,
+                              tight_card(
+                                "How Does The Size of a Confidence Interval Vary?",
+                                HTML("<p>
+                                     You might be wondering, what are the different features that can change the size of a confidence interval? There are lots! Toggle
+                                     the sections below to learn more.
+                                     </p>"),
+                                accordion(
+                                  open = FALSE,
+                                  
+                                  accordion_panel(
+                                    HTML("<b>Changing the Confidence Level</b>"),
+                                    HTML("<p>
+                                         In the example above, the confidence interval was 95%. We said that this means if we were to repeat the process of taking many
+                                         random samples from the population and calculating a 95% confidence interval, then about 95% of those intervals would contain
+                                         the true population mean.
+                                         <br><br>
+
+                                         Hence, if we were to change the confidence interval to something smaller, like 80%, then about 80% of those intervals would 
+                                         contain the true population mean. As fewer of the intervals contain the true population mean, the intervals are smaller than
+                                         those when the confidence level is 95%.
+                                         <br><br>
+                                         
+                                         If the confidence level were 99%, then about 99% of the intervals would contain the true population mean. To ensure that the 
+                                         intervals cover the true population mean, the intervals are larger. Hence, the 99% confidence intervals will be larger than 
+                                         the 95% ones.
+                                         <br><br>
+                                         
+                                         <b><span style='color: red;'>Key point:</span></b> In general, increasing the confidence level will increase the confidence 
+                                         interval size.
+                                         </p>"),
+                                  ),
+                                  accordion_panel(
+                                    HTML("<b>Changing the Sample Size</b>"),
+                                    HTML("<p>
+                                         The whole point of a confidence interval constructed from a sample mean is that we want to have an indication of where the 
+                                         population mean is. This is often desired in statistics because it is costly to collect data from everyone in a population.
+                                         <br><br>
+
+                                         As we collect more data from a population, we get closer and closer to capturing the entire population. For example, consider 
+                                         our population contained 1000 people, and we wanted to know whether they preferred Coke or Pepsi. If we interviewed 100 people,
+                                         we are still quite far from the entire population (we have only accounted for 10% of the data). However, if we interviewed 900
+                                         people, we are now quite close to the entire population (we have accounted for 90% of the data).
+                                         <br><br>
+                                         
+                                         In the case where we interviewed 90% of the population, there is less uncertainty about what the population mean is and hence
+                                         we would expect the confidence interval to be smaller than in the case where we only interviewed 10% of the population.
+                                         <br><br>
+                                         
+                                         <b><span style='color: red;'>Key point:</span></b> In general, increasing the sample size will decrease the confidence interval
+                                         size.
+                                         </p>"),
+                                  ),
+                                  accordion_panel(
+                                    HTML("<b>Changing the Hypothesis Test Type</b>"),
+                                    HTML("<p>
+                                         We will not focus on this too much here, but different hypothesis test types will have different-sized confidence intervals.
+                                         For example, a confidence interval for a t-test will typically be larger than that of a z-test (because t-curves have fatter
+                                         tails).
+                                         </p>"),
+                                  )
+                                ),
+                                HTML("<br>"),
+                                HTML("<p>
+                                     Your task - For a given normal distribution (we are taking samples from the specified distribution):
+                                     <ul>
+                                      <li>How does the size of the confidence intervals change when <b>adjusting the confidence level</b>?</li>
+                                      <li>How does the size of the confidence intervals change when <b>adjusting the sample size</b>?</li>
+                                     </ul>
+                                     <br>
+                                     <i>Note: Here, each confidence interval is taking from sampling from the specified normal distribution. This is for learning purposes only.
+                                     In reality, you would not know the true population mean or sample standard deviation (otherwise, what would be the point of contructing an
+                                     interval in the first place).</i>
+                                     </p>"),
+                                header_colour = "#3179ae"
+                              )
+                       ),
+                       column(7,
+                              primary_card(
+                                "Demonstration",
+                                fluidRow(
+                                  column(6,
+                                         HTML("<br>"),
+                                         sliderInput(
+                                           "confidence_level",
+                                           "Confidence Level",
+                                           min = 1,
+                                           max = 99,
+                                           value = 95,
+                                           width = "100%"
+                                         ),
+                                         sliderInput(
+                                           "sample_size",
+                                           "Sample Size",
+                                           min = 5,
+                                           max = 100,
+                                           value = 25,
+                                           width = "100%"
+                                         ),
+                                         HTML("<center>"),
+                                         actionButton(
+                                           inputId = "simulate_demo_2", label = HTML('<p>Simulate</p>'),
+                                           class = "btn btn-primary", style="color: #fff;", width = "40%"
+                                         ),
+                                         HTML("</center>"),
+                                  ),
+                                  column(1),
+                                  column(5,
+                                         HTML("<p><center><b>Specify Population Distribution (Normal Distribution):</b></center></p>"),
+                                         sliderInput(
+                                           "dist_mean",
+                                           "Mean",
+                                           min = -10,
+                                           max = 10,
+                                           value = 0,
+                                         ),
+                                         sliderInput(
+                                           "dist_sd",
+                                           "Standard Deviation",
+                                           min = 1,
+                                           max = 10,
+                                           value = 1
+                                         ),
+                                  )
+                                ),
+                                plotOutput("confidence_interval_plot_demo_2"),
+                                fluidRow(
+                                  column(9,
+                                         uiOutput("demo_2_confidence_interval_history")
+                                  ),
+                                  column(3,
+                                         HTML("<center><br>"),
+                                         actionButton(
+                                           inputId = "reset_history_demo_2", label = HTML('<p>Reset History</p>'),
+                                           class = "btn btn-danger", style="color: #fff;", width = "100%"
+                                         ),
+                                         HTML("</center>")
+                                  )
+                                ),
+                                header_colour = "#3179ae"
+                              )
+                       ),
+                       
+                     ),
+                     
+                     HTML("<br><br><br>")
+
            )
   ),
   
@@ -191,15 +340,13 @@ server <- function(input, output, session) {
   lower_bounds_demo_1 <- reactiveVal(c())
   upper_bounds_demo_1 <- reactiveVal(c())
   contains_mean_demo_1 <- reactiveVal(c())
-  current_index_for_plotting_demo_1 <- reactiveVal(0)
-  
+
   intervals_generated_so_far <- reactiveVal(0)
   intervals_not_containing_mean <- reactiveVal(0)
 
   # Simulate - Creating 100 confidence intervals.
   observeEvent(input$simulate_demo_1, {
-    current_index_for_plotting_demo_1(length(lower_bounds_demo_1) + 1)
-    
+
     # Create 100 confidence intervals using a normal distribution.
     true_mean = 100
     sample_size = 30
@@ -284,6 +431,133 @@ server <- function(input, output, session) {
       "</ul>"
     ))
   })
+  
+  ########## Demo Confidence Intervals 2 ########## 
+  
+  lower_bounds_demo_2 <- reactiveVal(c())
+  upper_bounds_demo_2 <- reactiveVal(c())
+  contains_mean_demo_2 <- reactiveVal(c())
+
+  intervals_generated_so_far_2 <- reactiveVal(0)
+  intervals_not_containing_mean_2 <- reactiveVal(0)
+  
+  # Simulate - Creating 100 confidence intervals.
+  observeEvent(input$simulate_demo_2, {
+
+    # Create 100 confidence intervals using a normal distribution.
+    true_mean = input$dist_mean
+    sample_size = input$sample_size
+    pop_sd = input$dist_sd
+    lower_bounds = numeric(100)
+    upper_bounds = numeric(100)
+    contains_mean = logical(100)
+    not_containing_mean_count = 0
+    for (i in 1:100) {
+      sample = rnorm(sample_size, mean = true_mean, sd = pop_sd)
+      sample_mean = mean(sample)
+      stderr = sd(sample) / sqrt(sample_size)
+      percentile = (1 + (input$confidence_level)/100)/2
+      margin = qnorm(percentile) * stderr
+      lower = sample_mean - margin
+      upper = sample_mean + margin
+      lower_bounds[i] = lower
+      upper_bounds[i] = upper
+      contains_mean[i] = lower <= true_mean & upper >= true_mean
+      not_containing_mean_count = not_containing_mean_count + !(lower <= true_mean & upper >= true_mean)
+    }
+    
+    # Save data
+    lower_bounds_demo_2(lower_bounds)
+    upper_bounds_demo_2(upper_bounds)
+    contains_mean_demo_2(contains_mean)
+    
+    # Update historical values
+    intervals_generated_so_far_2(intervals_generated_so_far_2() + 100)
+    intervals_not_containing_mean_2(intervals_not_containing_mean_2() + not_containing_mean_count)
+  })
+  
+  # Confidence interval plot.
+  output$confidence_interval_plot_demo_2 = renderPlot({
+    true_mean <- input$dist_mean
+    pop_sd <- max(input$dist_sd, 1e-6)
+    
+    # Define min and max confidence level allowed
+    conf_min <- 1
+    conf_max <- 99
+    
+    # Compute largest margin for fixed axis (at max confidence level & smallest sample size)
+    percentile_max <- (1 + conf_max / 100) / 2
+    stderr_max <- pop_sd / sqrt(5)
+    margin_max <- qnorm(percentile_max) * stderr_max
+    
+    # Fixed axis limits based on largest possible margin
+    x_min <- true_mean - margin_max * 1.2
+    x_max <- true_mean + margin_max * 1.2
+    
+    # Current confidence level (used for titles, actual CI calculation elsewhere)
+    conf_level <- min(max(input$confidence_level, conf_min), conf_max)
+    percentile <- (1 + conf_level / 100) / 2
+    
+    lower_bounds <- lower_bounds_demo_2()
+    upper_bounds <- upper_bounds_demo_2()
+    contains_mean <- contains_mean_demo_2()
+    
+    plot(
+      NA, xlim = c(x_min, x_max), ylim = c(1, 100),
+      xlab = "Value", ylab = "Sample Number",
+      main = paste0("Simulated ", conf_level, "% Confidence Intervals")
+    )
+    
+    abline(v = true_mean, lty = 2)
+    
+    if (length(lower_bounds) > 0) {
+      for (i in seq_along(lower_bounds)) {
+        color <- if (contains_mean[i]) "green" else "red"
+        lines(c(lower_bounds[i], upper_bounds[i]), c(i, i), col = color, lwd = 2)
+      }
+    }
+  })
+  
+  
+  # History of the confidence intervals that have been generated.
+  output$demo_2_confidence_interval_history <- renderUI({
+    total_intervals_generated_so_far = intervals_generated_so_far_2()
+    num_intervals_not_containing_mean = intervals_not_containing_mean_2()
+    percentage_containing_mean = 0
+    if (total_intervals_generated_so_far != 0) {
+      percentage_containing_mean = 100 * (total_intervals_generated_so_far-num_intervals_not_containing_mean)/total_intervals_generated_so_far
+    }
+    HTML(paste0(
+      "<b>Running History:</b><br>",
+      "<ul>",
+      "<li>Total number of intervals generated so far: ", total_intervals_generated_so_far, "</li>",
+      "<li>Total number of intervals NOT containing the population mean: ", num_intervals_not_containing_mean, "</li>",
+      "<li>Percentage of intervals containing the population mean: ", round(percentage_containing_mean, 2), "%</li>",
+      "</ul>"
+    ))
+  })
+  
+  observeEvent(input$reset_history_demo_2, {
+    intervals_generated_so_far_2(0)
+    intervals_not_containing_mean_2(0)
+  })
+  observeEvent(input$dist_mean, {
+    intervals_generated_so_far_2(0)
+    intervals_not_containing_mean_2(0)
+  })
+  observeEvent(input$dist_sd, {
+    intervals_generated_so_far_2(0)
+    intervals_not_containing_mean_2(0)
+  })
+  observeEvent(input$confidence_level, {
+    intervals_generated_so_far_2(0)
+    intervals_not_containing_mean_2(0)
+  })
+  observeEvent(input$sample_size, {
+    intervals_generated_so_far_2(0)
+    intervals_not_containing_mean_2(0)
+  })
+  
   
   boxModelPart1Server(id = "box_model_part_1")
   boxModelPart2Server(id = "box_model_part_2")
